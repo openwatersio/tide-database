bdc19058df46f11caf86b7b22ecce0353b22bd11
Brandon Keepers <brandon@opensoul.org>
2026-02-17 14:33:45 -0500

    Fix a few NOAA stations that don't have MLLW

--- tools/update-noaa-stations.ts
+++ tools/update-noaa-stations.ts
@@ -104,7 +104,19 @@ async function buildStation(meta: any): Promise<StationData> {
       }
     }
 
+    const datums = {
+      ...(data.datums.LAT ? { LAT: data.datums.LAT } : {}),
+      ...(data.datums.HAT ? { HAT: data.datums.HAT } : {}),
+      // Some stations don't have all datums
+      ...(data.datums.datums
+        ? Object.fromEntries(
+            data.datums.datums.map((d: any) => [d.name, d.value]),
+          )
+        : {}),
+    };
+
     Object.assign(station, {
+      chart_datum: "MLLW" in datums ? "MLLW" : "STND",
       harmonic_constituents: data.harmonicConstituents.HarmonicConstituents.map(
         (h: any) => ({
           name: h.name,
@@ -112,16 +124,7 @@ async function buildStation(meta: any): Promise<StationData> {
           phase: h.phase_GMT,
         }),
       ),
-      datums: {
-        ...(data.datums.LAT ? { LAT: data.datums.LAT } : {}),
-        ...(data.datums.HAT ? { HAT: data.datums.HAT } : {}),
-        // Some stations don't have all datums
-        ...(data.datums.datums
-          ? Object.fromEntries(
-              data.datums.datums.map((d: any) => [d.name, d.value]),
-            )
-          : {}),
-      },
+      datums,
       disclaimers: (data.disclaimers.disclaimers ?? [])
         .map((d: any) => d.text)
         .join("\n"),
